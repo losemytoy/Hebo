e86f21fea6d3661c91d294595fae373296f73462
linyiming <2391660155@qq.com>
2023-07-19 14:45:29 +0100

    2023/07/19: resunet_selfAttention

--- MyModel/Models/ResUNet.py
+++ MyModel/Models/ResUNet.py
@@ -1,13 +1,13 @@
 from torch import nn
 import torch
 import torchvision.models as models
+import torch.nn.functional as F
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import timm
+from einops import rearrange, repeat
 
 
 class ConvBlock(nn.Module):
-    """
-    Helper module that consists of a Conv -> BN -> ReLU
-    """
-
     def __init__(self, in_channels, out_channels, padding=1, kernel_size=3, stride=1):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, padding=padding, kernel_size=kernel_size, stride=stride)
@@ -40,7 +40,8 @@ class DecoderBlock(nn.Module):
         self.mid_channels = mid_channels
         self.out_channels = out_channels
         self.upsample_mode = upsample_mode
-
+        # define self-attention
+        self.attn = GlobalLocalAttention(dim=in_channels, num_heads=8, window_size=8)
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=3, padding=1, bias=False)
         self.norm1 = nn.BatchNorm2d(mid_channels)
         self.relu1 = nn.ReLU()
@@ -54,6 +55,7 @@ class DecoderBlock(nn.Module):
         self.relu2 = nn.ReLU()
 
     def forward(self, x):
+        x = self.attn(x)
         x = self.conv(x)
         x = self.norm1(x)
         x = self.relu1(x)
@@ -63,12 +65,138 @@ class DecoderBlock(nn.Module):
         return x
 
 
+class ConvBN(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1, norm_layer=nn.BatchNorm2d,
+                 bias=False):
+        super(ConvBN, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=bias,
+                      dilation=dilation, stride=stride, padding=((stride - 1) + dilation * (kernel_size - 1)) // 2),
+            norm_layer(out_channels)
+        )
+
+
+class Conv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1, bias=False):
+        super(Conv, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=bias,
+                      dilation=dilation, stride=stride, padding=((stride - 1) + dilation * (kernel_size - 1)) // 2)
+        )
+
+
+class SeparableConvBN(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, dilation=1,
+                 norm_layer=nn.BatchNorm2d):
+        super(SeparableConvBN, self).__init__(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, dilation=dilation,
+                      padding=((stride - 1) + dilation * (kernel_size - 1)) // 2,
+                      groups=in_channels, bias=False),
+            norm_layer(out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        )
+
+
+class GlobalLocalAttention(nn.Module):
+    def __init__(self,
+                 dim=256,
+                 num_heads=16,
+                 qkv_bias=False,
+                 window_size=8,
+                 relative_pos_embedding=True
+                 ):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // self.num_heads
+        self.scale = head_dim ** -0.5
+        self.ws = window_size
+
+        self.qkv = Conv(dim, 3 * dim, kernel_size=1, bias=qkv_bias)
+        self.local1 = ConvBN(dim, dim, kernel_size=3)
+        self.local2 = ConvBN(dim, dim, kernel_size=1)
+        self.proj = SeparableConvBN(dim, dim, kernel_size=window_size)
+
+        self.attn_x = nn.AvgPool2d(kernel_size=(window_size, 1), stride=1, padding=(window_size // 2 - 1, 0))
+        self.attn_y = nn.AvgPool2d(kernel_size=(1, window_size), stride=1, padding=(0, window_size // 2 - 1))
+
+        self.relative_pos_embedding = relative_pos_embedding
+
+        if self.relative_pos_embedding:
+            # define a parameter table of relative position bias
+            self.relative_position_bias_table = nn.Parameter(
+                torch.zeros((2 * window_size - 1) * (2 * window_size - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+            # get pair-wise relative position index for each token inside the window
+            coords_h = torch.arange(self.ws)
+            coords_w = torch.arange(self.ws)
+            coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+            coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+            relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+            relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+            relative_coords[:, :, 0] += self.ws - 1  # shift to start from 0
+            relative_coords[:, :, 1] += self.ws - 1
+            relative_coords[:, :, 0] *= 2 * self.ws - 1
+            relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+            self.register_buffer("relative_position_index", relative_position_index)
+
+            trunc_normal_(self.relative_position_bias_table, std=.02)
+
+    def pad(self, x, ps):
+        _, _, H, W = x.size()
+        if W % ps != 0:
+            x = F.pad(x, (0, ps - W % ps), mode='reflect')
+        if H % ps != 0:
+            x = F.pad(x, (0, 0, 0, ps - H % ps), mode='reflect')
+        return x
+
+    def pad_out(self, x):
+        x = F.pad(x, pad=(0, 1, 0, 1), mode='reflect')
+        return x
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+
+        local = self.local2(x) + self.local1(x)
+        # global => q, k, v
+        x = self.pad(x, self.ws)
+        B, C, Hp, Wp = x.shape
+        qkv = self.qkv(x)
+
+        q, k, v = rearrange(qkv, 'b (qkv h d) (hh ws1) (ww ws2) -> qkv (b hh ww) h (ws1 ws2) d', h=self.num_heads,
+                            d=C // self.num_heads, hh=Hp // self.ws, ww=Wp // self.ws, qkv=3, ws1=self.ws, ws2=self.ws)
+
+        dots = (q @ k.transpose(-2, -1)) * self.scale
+
+        if self.relative_pos_embedding:
+            relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
+                self.ws * self.ws, self.ws * self.ws, -1)  # Wh*Ww,Wh*Ww,nH
+            relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+            dots += relative_position_bias.unsqueeze(0)
+
+        attn = dots.softmax(dim=-1)
+        attn = attn @ v
+
+        attn = rearrange(attn, '(b hh ww) h (ws1 ws2) d -> b (h d) (hh ws1) (ww ws2)', h=self.num_heads,
+                         d=C // self.num_heads, hh=Hp // self.ws, ww=Wp // self.ws, ws1=self.ws, ws2=self.ws)
+
+        attn = attn[:, :, :H, :W]
+
+        out = self.attn_x(F.pad(attn, pad=(0, 0, 0, 1), mode='reflect')) + \
+              self.attn_y(F.pad(attn, pad=(0, 1, 0, 0), mode='reflect'))
+
+        out = out + local
+        out = self.pad_out(out)
+        out = self.proj(out)
+        # print(out.size())
+        out = out[:, :, :H, :W]
+
+        return out
+
+
 class ResUNet(nn.Module):
     def __init__(self, num_classes):
         super(ResUNet, self).__init__()
-        resnet = models.resnet50('IMAGENET1K_V1')
+        resnet = models.resnet50()
         filters = [64, 256, 512, 1024, 2048]
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(in_channels=5, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = resnet.bn1
         self.relu1 = resnet.relu
         self.maxpool = resnet.maxpool
